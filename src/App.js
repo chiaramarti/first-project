de5884f612c8a87f5a4fb01c59d62a878319c74c
chiaramarti <martinellichiara94@gmail.com>
2024-03-11 17:28:05 +0100

    first commit - react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FirstBtn from "./component/Btn";
+import ImageComponent from "./component/ImageComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <FirstBtn textBtn="Hi! Don't click me.." />
+        <FirstBtn textBtn="Hi! Don't click me.." class={"tiffany"} />
+        <ImageComponent src="https://picsum.photos/seed/picsum/200/300/" alt="image picsum" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/component/Btn.jsx
@@ -0,0 +1,9 @@
+const FirstBtn = function (props) {
+    return (
+        <div>
+            <button className={props.class}>{props.textBtn}</button>
+        </div>
+    )
+}
+
+export default FirstBtn
